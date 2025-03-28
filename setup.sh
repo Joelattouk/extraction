c6bf330adddffacc768e17c17a90c8ebc80595a9
Joelattouk <82134532+Joelattouk@users.noreply.github.com>
2025-03-28 15:50:17 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-# Mise à jour des paquets système et installation des dépendances nécessaires pour Selenium
+# Mise à jour et installation de Chromium + drivers
 apt-get update
-apt-get install -y libglib2.0-0 libsm6 libxext6 libxrender1
+apt-get install -y --no-install-recommends \
+    chromium \
+    chromium-driver \
+    libglib2.0-0 \
+    libsm6 \
+    libxext6 \
+    libxrender1
 
-# Installer les dépendances Python spécifiées dans requirements.txt
-pip install -r requirements.txt
-# Ajoutez ces lignes
-apt-get install -y chromium chromium-driver
+# Installation des dépendances Python
+pip install --no-cache-dir -r requirements.txt
